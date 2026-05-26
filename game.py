77738e5edc39e15200761ed9cf39fde9edb55866
g.des <yung710@mail.ru>
2026-05-26 22:22:10 +0500

    поменял стиль игры, добавил спавн жуков раз в 0.8 секунд, добавил алгоритм обнаружения турелями жуков

--- game.py
+++ game.py
@@ -5,12 +5,12 @@ import sys
 WIDTH, HEIGHT = 800, 600
 TILE_SIZE = 40
 
-# Цветовая палитра Марса
-COLOR_SAND = (175, 80, 50)       # Ржавый песок
-COLOR_PATH = (100, 40, 20)       # Глубокий каньон (дорога)
-COLOR_GRID = (190, 95, 60)       # Линии сетки
-COLOR_BUG = (150, 255, 100)      # Кислотно-зеленый пришелец
-COLOR_DOME = (50, 150, 255)      # Купол базы
+# Цветовая Звездного десанта
+COLOR_KLENDATHU_ROCK = (80, 60, 50)      # Грунт
+COLOR_CANYON = (40, 30, 25)              # Глубокий каньон (Дорога для жуков)
+COLOR_GRID = (100, 80, 70)               # сетка
+COLOR_ARACHNID = (255, 120, 0)           # Жуки
+COLOR_OUTPOST = (150, 150, 150)          # Аванпост
 
 # Маршрут врагов (Координаты центров тайлов)
 WAYPOINTS = [
@@ -23,7 +23,8 @@ class AlienBug(pygame.sprite.Sprite):
         super().__init__(*groups)
         self.image = pygame.Surface((30, 30), pygame.SRCALPHA)
         # Рисуем жука 
-        pygame.draw.circle(self.image, COLOR_BUG, (15, 15), 15)
+        pygame.draw.circle(self.image, COLOR_ARACHNID, (15, 15), 15)
+        pygame.draw.circle(self.image, (0, 0, 0), (15, 15), 10, 2)
         self.rect = self.image.get_rect()
         
         self.waypoints = waypoints
@@ -33,10 +34,10 @@ class AlienBug(pygame.sprite.Sprite):
         self.pos = pygame.math.Vector2(self.waypoints[self.current_wp_index])
         self.rect.center = (round(self.pos.x), round(self.pos.y))
         
-        self.speed = 150  # пикселей в секунду
+        self.speed = 190  # пикселей в секунду
 
     def update(self, dt):
-        # Если достигли конца маршрута (Купола)
+        # Если достигли конца маршрута (аванпоста)
         if self.current_wp_index >= len(self.waypoints):
             self.kill() # Жук исчезает (потом добавлю нанесение урона базе)
             return
@@ -69,31 +70,46 @@ class Turret(pygame.sprite.Sprite):
         pygame.draw.rect(self.image, (200, 200, 200), (16, 4, 8, 16))
 
         self.rect = self.image.get_rect(center=pos)
+        self.pos = pygame.Vector2(pos)
 
         # характеристики для стрельбы
         self.radius = 100 # обзор
-        self.cooldown = 1.0 # скорость стрельбы
-        self.current_time = 0.0
+        self.target = None # цель
     
-    def update(self, dt):
-        pass
+    def find_target(self, bugs_group):
+        closest_bug = None
+        min_dist = self.radius
 
-class MarsBaseGame:
+        for bug in bugs_group:
+            dist = self.pos.distance_to(bug.rect.center)
+            if dist < min_dist:
+                min_dist = dist
+                closest_bug = bug
+
+        return closest_bug
+    
+    def update(self, dt, bugs_group):
+        self.target = self.find_target(bugs_group)
+
+class OutpostDefenseGame:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-        pygame.display.set_caption("Mars Base Defense")
+        pygame.display.set_caption("Whiskey Outpost")
         self.clock = pygame.time.Clock()
         self.running = True
 
         self.all_sprites = pygame.sprite.Group()
         self.turrets_group = pygame.sprite.Group()
+        self.bugs_group = pygame.sprite.Group()
         
+        self.spawn_timer = 0.0
+        self.spawn_interval = 0.8
         # Спавним тестового жука
         AlienBug(WAYPOINTS, self.all_sprites)
 
     def draw_grid_and_path(self):
-        self.screen.fill(COLOR_SAND)
+        self.screen.fill(COLOR_KLENDATHU_ROCK)
         
         # отрисовка сетки
         for x in range(0, WIDTH, TILE_SIZE):
@@ -103,23 +119,51 @@ class MarsBaseGame:
 
         # отрисовка дороги
         if len(WAYPOINTS) > 1:
-            pygame.draw.lines(self.screen, COLOR_PATH, False, WAYPOINTS, TILE_SIZE)
+            pygame.draw.lines(self.screen, COLOR_CANYON, False, WAYPOINTS, TILE_SIZE)
 
-        # отрисовка купола на последней точке маршрута
+        # отрисовка базы на последней точке маршрута
         dome_pos = WAYPOINTS[-1]
-        pygame.draw.circle(self.screen, COLOR_DOME, dome_pos, 40)
+        pygame.draw.circle(self.screen, COLOR_OUTPOST, dome_pos, 40)
 
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                mouse_x, mouse_y = event.pos
+                grid_x = (mouse_x // TILE_SIZE) * TILE_SIZE
+                grid_y = (mouse_y // TILE_SIZE) * TILE_SIZE
+                center_pos = (grid_x + TILE_SIZE // 2, grid_y + TILE_SIZE // 2)
+                
+                temp_rect = pygame.Rect(grid_x, grid_y, TILE_SIZE, TILE_SIZE)
+                
+                # проверка, чтобы не ставить турель на другую
+                if not any(t.rect.colliderect(temp_rect) for t in self.turrets_group):
+                    Turret(center_pos, self.all_sprites, self.turrets_group)
 
     def update(self, dt):
-        self.all_sprites.update(dt)
+        # спавн жуков
+        self.spawn_timer += dt
+        if self.spawn_timer >= self.spawn_interval:
+            AlienBug(WAYPOINTS, self.all_sprites, self.bugs_group)
+            self.spawn_timer = 0.0 # Сброс таймера
+            
+        # обновление жуков
+        self.bugs_group.update(dt)
+        
+        # обновление турелей
+        for turret in self.turrets_group:
+            turret.update(dt, self.bugs_group)
 
     def draw(self):
         self.draw_grid_and_path()
         self.all_sprites.draw(self.screen)
+        
+        # лазерный прицела для наглядности работы алгоритма
+        for turret in self.turrets_group:
+            if turret.target:
+                pygame.draw.line(self.screen, (255, 0, 0), turret.rect.center, turret.target.rect.center, 2)
+                
         pygame.display.flip()
 
     def run(self):
@@ -133,4 +177,4 @@ class MarsBaseGame:
         sys.exit()
 
 if __name__ == "__main__":
-    MarsBaseGame().run()
+    OutpostDefenseGame().run()
